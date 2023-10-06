181b367d0668325d522b1a2455aacb69ad9621a5
nravesz <nraveszani@gmail.com>
2023-10-06 11:49:07 -0300

    feature (router): now the router is configured

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Home } from "./components/home";
+import { HomePage, ContactPage } from './pages';
 
 function App() {
   return (
-    <div className="App">
-      <p>pipipi</p>
-    </div>
+    // <div className="App">
+    //   <Home />
+    // </div>
+    <BrowserRouter>
+      <Routes>
+        <Route>
+          <Route index element={<HomePage />} />
+          <Route path="home" element={<HomePage />} />
+          <Route path="contact" element={<ContactPage />} />
+        </Route>
+      </Routes>
+  </BrowserRouter>
   );
 }
 
---
+++ front/src/components/home/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./styles/Home.scss";
+
+const Home = () => {
+    return (
+        <div className="home-div">
+            <p className="title-text">pipipi</p>
+        </div>
+    );
+};
+
+export default Home;
