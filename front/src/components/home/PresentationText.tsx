ae8ce4bcb745ee1936003783fb89d121de7cf952
nravesz <nraveszani@gmail.com>
2023-10-11 00:12:46 -0300

    style (Home)

--- front/src/components/home/PresentationText.tsx
+++ front/src/components/home/PresentationText.tsx
@@ -5,7 +5,15 @@ const PresentationText = () => {
         <div>
             <p className="greetings-text"> Hi! </p>
             <p className="name-text"> I'm Nicole Raveszani </p>
-            <p className="stack-text"> A Full Stack Developer </p>
+            <div>
+                {/* <p className="stack-text"> A Full Stack Developer </p>
+                <p className="stack-text"> A Full Stack Developer </p> */}
+                <div className="glitch-text-div">
+                    <div className="glitch-text-1"> A Full Stack Developer </div>
+                    <div className="glitch-text-2"> A Full Stack Developer </div>
+                    <div className="glitch-text-3"> A Full Stack Developer </div>
+                </div>
+            </div>
         </div>
     );
 };
