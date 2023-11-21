e2c5875e97d5a18dd386c7e6973b27fa644d7063
nravesz <nraveszani@gmail.com>
2023-11-21 16:31:50 -0300

    redesign (Home): remove the old design

---
+++ front/src/components/home/TitleText.tsx
@@ -0,0 +1,11 @@
+import "./styles/TitleText.scss";
+
+const TitleText = () => {
+    return (
+        <div>
+            <p> Nicole Raveszani </p>
+        </div>
+    );
+};
+
+export default TitleText;
