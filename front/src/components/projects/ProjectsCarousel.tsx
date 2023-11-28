09304cfd03340d82486dc81057bcfcb21a6505be
nravesz <nraveszani@gmail.com>
2023-11-28 14:57:46 -0300

    its horrible but okay

--- front/src/components/projects/ProjectsCarousel.tsx
+++ front/src/components/projects/ProjectsCarousel.tsx
@@ -38,58 +38,40 @@ const projects: IProject[] = [
 ];
 
 const ProjectsCarousel = () => {
-    const [currentSlide, setCurrentSlide] = useState(0);
+  const [currentSlide, setCurrentSlide] = useState(0);
 
-    const nextSlide = () => {
-      setCurrentSlide((prevSlide) => (prevSlide + 1) % 3);
-    };
-  
-    const prevSlide = () => {
-      setCurrentSlide((prevSlide) => (prevSlide - 1 + 3) % 3);
-    };
-  
-    return (
-      <div className="carousel-container">
-        <div className="carousel-slides" style={{ transform: `translateX(-${currentSlide * 33.33}%)` }}>
-          {/* Your carousel slides go here */}
-          <div className={`carousel-slide`}
-          >
-            <div
-              className={`item ${currentSlide == 4 ? "current" : ""}`}
-              style={{transform: `${currentSlide == 4 ? "scale(2)" : "scale(1)"}`}}
-            >
-                Project 1
-            </div>
-          </div>
-          <div className={`carousel-slide`}>
-            <div className={`item ${currentSlide == 0 ? "current" : ""}`}
-            >
-                Project 2
-            </div>
-           </div>
-          <div className={`carousel-slide`}>
-            <div className={`item ${currentSlide == 1 ? "current" : ""}`}>
-                Project 3
-            </div>
-          </div>
-          <div className={`carousel-slide`}>
-            <div className={`item ${currentSlide == 2 ? "current" : ""}`}>
-                Project 4
-            </div>
-          </div>
-          <div className={`carousel-slide`}>
-            <div className={`item ${currentSlide == 3 ? "current" : ""}`}>Project 5</div>
-          </div>
-          {/* Add more slides as needed */}
-        </div>
-        <button className="carousel-button" onClick={prevSlide}>
-          Prev
-        </button>
-        <button className="carousel-button" onClick={nextSlide}>
-          Next
-        </button>
-      </div>
-    );
+  const nextSlide = () => {
+    setCurrentSlide((prevSlide) => (prevSlide + 1) % projects.length);
   };
+
+  const prevSlide = () => {
+    setCurrentSlide((prevSlide) => (prevSlide - 1 + projects.length) % projects.length);
+  };
+
+  return (
+    <div className="carousel-container">
+      <div className="carousel-slides" style={{ transform: `translateX(-${currentSlide * 33.33}%)` }}>
+        {projects.map((project, index) => (
+          <div
+            key={index}
+            className={`carousel-slide ${currentSlide === index ? "current" : ""}`}
+            style={{ transform: `scale(${currentSlide === index ? 1.5 : 1})` }}
+          >
+            <div className="item">
+              {/* Project content goes here */}
+              {project.name}
+            </div>
+          </div>
+        ))}
+      </div>
+      <button className="carousel-button" onClick={prevSlide}>
+        Prev
+      </button>
+      <button className="carousel-button" onClick={nextSlide}>
+        Next
+      </button>
+    </div>
+  );
+};
 
 export default ProjectsCarousel;
