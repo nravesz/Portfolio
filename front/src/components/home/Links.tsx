b9e8ea2db693c3a8a70283ec53bc9180a3e9aefa
nravesz <nraveszani@gmail.com>
2024-01-09 23:36:01 -0300

    details in home

--- front/src/components/home/Links.tsx
+++ front/src/components/home/Links.tsx
@@ -3,9 +3,12 @@ import "./styles/Links.scss";
 
 const Links = () => {
     return (
-        <div className="links-div">
-            <p>You can find me here:</p>
-            <SitesIcons />
+        <div>
+            <div className="links-div">
+                <p>You can find me here:</p>
+                <SitesIcons />
+            </div>
+                <p>Or you can send me an email: <span>nraveszani@gmail.com</span></p>
         </div>
     );
 };
